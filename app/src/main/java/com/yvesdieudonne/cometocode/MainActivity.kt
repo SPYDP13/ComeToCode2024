a7d4f0210b85d9ce6631551c48cf921c644f7bc8
SPYDP13 <yvesdieudonnesawadogo354@gmail.com>
2024-06-13 14:20:02 +0000

    2

--- app/src/main/java/com/yvesdieudonne/cometocode/MainActivity.kt
+++ app/src/main/java/com/yvesdieudonne/cometocode/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.yvesdieudonne.cometocode
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yvesdieudonne.cometocode.Home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Intent(this, HomeActivity::class.java).also {
+            startActivity(it)
+        }
     }
 }
