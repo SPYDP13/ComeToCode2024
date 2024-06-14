ed2894c17566ff836c29800f07d77499eacd43a6
SPYDP13 <yvesdieudonnesawadogo354@gmail.com>
2024-06-14 01:22:58 +0000

    7

--- app/src/main/java/com/yvesdieudonne/cometocode/Personne/PersonnesRecherche/DeclarationPersonneRecherche/DeclarationPersonneRecherche.kt
+++ app/src/main/java/com/yvesdieudonne/cometocode/Personne/PersonnesRecherche/DeclarationPersonneRecherche/DeclarationPersonneRecherche.kt
@@ -2,11 +2,44 @@ package com.yvesdieudonne.cometocode.Personne.PersonnesRecherche.DeclarationPers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import com.yvesdieudonne.cometocode.R
 
 class DeclarationPersonneRecherche : AppCompatActivity() {
+    lateinit var menuIV: ImageView
+    lateinit var menuCL: ConstraintLayout
+    lateinit var closeMenuIV: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_declaration_personne_recherche)
+        menuIV = findViewById(R.id.menuIV)
+        menuCL= findViewById(R.id.menuCL)
+        closeMenuIV= findViewById(R.id.closeMenuIV)
+
+
+        /*Actionnement du menu*/
+        menuIV.setOnClickListener{
+            YoYo.with(Techniques.BounceInLeft)
+                .duration(1000)
+                .onStart { menuCL.visibility = View.VISIBLE }
+                .playOn(menuCL)
+        }
+        closeMenuIV.setOnClickListener{
+            YoYo.with(Techniques.FadeOutLeft)
+                .duration(1000)
+                .onEnd { menuCL.visibility = View.GONE }
+                .playOn(menuCL)
+        }
+        menuCL.setOnClickListener{
+            YoYo.with(Techniques.FadeOutLeft)
+                .duration(1000)
+                .onEnd { menuCL.visibility = View.GONE }
+                .playOn(menuCL)
+        }
+
     }
 }
